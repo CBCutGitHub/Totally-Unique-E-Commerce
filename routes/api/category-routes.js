0d6fbbc3d3388424c9d6b58fcf2e4295ba6a042f
MDemp <mdempsey.cbc.ut@gmail.com>
2022-02-09 17:48:57 -0600

    Added develop folder and adjust env and adjusted process env in connection.js

---
+++ routes/api/category-routes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  
+});
+
+router.get('/:id', (req, res) => {
+  
+});
+
+router.post('/', (req, res) => {
+  
+});
+
+router.put('/:id', (req, res) => {
+  
+});
+
+router.delete('/:id', (req, res) => {
+ 
+});
+
+module.exports = router;
