82a2079b608b78d57187bae5f8c928c42e76448e
MDemp <mdempsey.cbc.ut@gmail.com>
2022-02-09 18:04:37 -0600

    Added find-config, models, seeded db.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 app.use(routes);
 
