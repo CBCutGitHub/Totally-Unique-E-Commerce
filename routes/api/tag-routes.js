9760d98844d89355e1ce024edf2751d19c238cc8
MDemp <mdempsey.cbc.ut@gmail.com>
2022-03-10 16:27:56 -0600

    Add get, post, put and delete for tags.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -2,23 +2,59 @@ const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
-  
+    Tag.findAll({
+        include: [{ model: Product, through: ProductTag }]
+    }).then(function (response) {
+        res.json(response)
+    }).catch(function (err) {
+        res.json(err)
+    })
+
 });
 
 router.get('/:id', (req, res) => {
-  
+    Tag.findOne({
+        where: { id: req.params.id },
+        include: [{ model: Product, through: ProductTag }]
+    }).then(function (response) {
+        res.json(response)
+    }).catch(function (err) {
+        res.json(err)
+    })
 });
 
 router.post('/', (req, res) => {
-  
+    Tag.create(req.body)
+        .then(function (response) {
+            res.json(response)
+
+        }).catch(function (err) {
+            res.json(err)
+        })
 });
 
 router.put('/:id', (req, res) => {
-  
+    Tag.update(req.body,
+        { where: { id: req.params.id } })
+        .then(function (response) {
+            res.json(response)
+
+        }).catch(function (err) {
+            res.json(err)
+        })
+
 });
 
 router.delete('/:id', (req, res) => {
-  
+    Tag.destroy(
+        { where: { id: req.params.id } })
+        .then(function (response) {
+            res.json(response)
+
+        }).catch(function (err) {
+            res.json(err)
+        })
+
 });
 
 module.exports = router;
